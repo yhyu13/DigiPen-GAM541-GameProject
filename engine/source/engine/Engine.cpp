967aabf5fa7ca1031a6ae103c4e54701005a4bf1
Dushyant Shukla <dushyantshukla@outlook.com>
2020-01-28 21:15:25 -0800

    Frame rate controller (Dushyant Shukla). Updated the formatting of file headers.

--- engine/source/engine/Engine.cpp
+++ engine/source/engine/Engine.cpp
@@ -2,17 +2,18 @@
 Copyright (C) 2020 DigiPen Institute of Technology.
 Reproduction or disclosure of this file or its contents without the
 prior written consent of DigiPen Institute of Technology is prohibited.
-Language: c++ 11
-Platform: Windows 10 (X64)
-Project: GAM541
-Filename: Engine.cpp
-Purpose: Abstraction of the game loop.
-Author: Dushyant Shukla (dushyant.shukla@digipen.edu | 60000519)
-Creation date: 01/26/2020
+Language		: C++ 11
+Platform		: Windows 10 (X64)
+Project			: GAM541
+Filename		: Engine.cpp
+Purpose			: Abstraction of the game loop.
+Author			: Dushyant Shukla (dushyant.shukla@digipen.edu | 60000519)
+Creation date	: 01/26/2020
 - End Header ----------------------------*/
 
 #include "Engine.h"
 #include "engine/window/Window.h"
+#include "engine/framerate-controller/FramerateController.h"
 
 #include <iostream>
 
@@ -27,13 +28,16 @@ namespace gswy {
 	}
 
 	void Engine::Run() {
-	
+		FramerateController* rateController = FramerateController::GetInstance(60);
 		while (m_isRunning) {
+			rateController->FrameStart();
 			std::cout << "App is running..." << std::endl;
+			std::cout << "Frame time: " << rateController->GetFrameTime();
 			m_window->Update();
 			m_isRunning = !m_window->ShouldExit();
+			rateController->FrameEnd();
 		}
-	
+		delete rateController;
 	}
 
 }
