ae4be271fcfe730de1c58278a855657411be1055
Dushyant Shukla <dushyantshukla@outlook.com>
2020-02-07 01:11:10 -0800

    Added ECS headers to EngineExport.h

--- engine/source/EngineExport.h
+++ engine/source/EngineExport.h
@@ -38,3 +38,12 @@ Creation date	: 01/26/2020
 #include "engine/renderer/Renderer2D.h"
 #include "engine/renderer/Shader.h"
 #include "engine/renderer/OrthographicCameraController.h"
+
+
+///// Entity Component System /////
+#include "engine/ecs/BaseComponentSystem.h"
+#include "engine/ecs/BaseComponent.h"
+#include "engine/ecs/ComponentDecorator.h"
+#include "engine/ecs/GameWorld.h"
+#include "engine/ecs/EntityManager.h"
+#include "engine/ecs/EntityDecorator.h"
