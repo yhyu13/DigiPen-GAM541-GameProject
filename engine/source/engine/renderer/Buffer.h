67c3a2a48f97b27bdb833217917bb99cca2cd51c
KyleWang <z82101096@hotmail.com>
2020-02-15 00:54:05 -0800

    Fixed the problem that vertex buffer create every frame

--- engine/source/engine/renderer/Buffer.h
+++ engine/source/engine/renderer/Buffer.h
@@ -123,6 +123,7 @@ namespace gswy {
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
+		virtual void UpdateBufferData(float* vertices, uint32_t size) = 0;
 		virtual const BufferLayout& GetLayout() const = 0;
 		virtual void SetLayout(const BufferLayout& layout) = 0;
 
